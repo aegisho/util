7aed4d243c8bec88795c7d56fe1aaa558bfb5c04
yjj <yjj@163.com>
2015-09-02 08:56:11 +0800

    add

---
+++ DataSource.js
@@ -0,0 +1,168 @@
+﻿//数据处理
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['core', 'jquery'], factory)
+    } else {
+        factory()
+    }
+}(function (core, $) {
+    'use strict'
+
+    //模拟数据预加载
+    function PreLoad(options, ajaxdata) {
+        this.init(options, ajaxdata)
+        this.loadTime = 0
+    }
+
+    PreLoad.prototype = {
+        constructor: PreLoad,
+        init: function (options, ajaxdata) {
+            var value = { data: [], count: 0 },
+                mock = options
+
+            if (options.mock) {
+                mock = options.mock
+            }
+
+            if (typeof mock === 'function') {
+                mock = mock(ajaxdata)
+            }
+
+            if (mock instanceof Array) {
+                value = {
+                    data: mock,
+                    count: mock.length
+                }
+            } else if (mock.data && mock.count) {
+                value = mock
+            }
+
+            this.mock = value
+            this.times = options.times || 1
+        },
+        load: function (func) {
+            var mock = this.mock
+            if (mock && (this.times === 'always' || this.loadTime < this.times)) {
+                func(mock.data, mock.count, true)
+                this.loadTime++
+            }
+        }
+    }
+
+    //缓存
+    function Cache() {
+
+    }
+
+    Cache.prototype = {
+        constructor: Cache,
+        add: function (key, data) {
+            this[key] = data
+        },
+        get: function (key) {
+            return this[key]
+        },
+        remove: function (key) {
+            if (key) {
+                delete this[key]
+            } else {
+                this.remove()
+            }
+        },
+        clear: function () {
+            for (var key in this) {
+                if (this.hasOwnProperty(key)) {
+                    delete this[key]
+                }
+            }
+        },
+        update: function (func, key) {
+            //更新缓存，如果存在parse函数，请手动执行
+            var data
+            if (key) {
+                data = this[key]
+                func.call(this, data)
+            } else {
+                for (var key in this) {
+                    if (this.hasOwnProperty(key)) {
+                        data = this[key]
+                        func.apply(this, data)
+                    }
+                }
+            }
+        }
+    }
+
+    function DataSource(options) {
+        /// <summary>数据集</summary>
+        /// <param name="options" type="Object">参数</param>
+
+        var config = {
+            cache: true,    //查询缓存            
+            preload: false, //预加载
+            parse: null     //数据处理 parse应该是幂等函数
+        }
+
+        core.mixin(config, options)
+        core.mixin(this, config)
+
+        if (this.cache) {
+            this.cache = new Cache()
+        }
+    }
+
+    DataSource.prototype = {
+        constructor: DataSource,
+        setOptions: function (options) {
+            core.mixin(this, options)
+        },
+        ajax: function (options, fields) {
+            var successFunc = options.success,
+                key
+
+            //使用查询条件作为缓存key
+            if (this.cache) {
+                key = JSON.stringify(options.data)
+            }
+
+            options.success = function () {
+
+                //处理函数 如果为缓存的数据，不再使用parse
+                if (this.parse && !this.cache.get(key)) {
+                    this.parse.apply(null, arguments)
+                }
+
+                successFunc.apply(null, arguments)
+
+                //添加缓存             
+                if (this.cache) {
+                    //预加载不加入到缓存中
+                    var isPreload = arguments[2]
+                    if (!isPreload) {
+                        this.cache.add(key, Array.prototype.slice.call(arguments))
+                    }
+                }
+            }.bind(this)
+
+            //预加载模式
+            if (this.preload) {
+                if (!(this.preload instanceof PreLoad)) {
+                    this.preload = new PreLoad(this.preload, options.data)
+                }               
+                this.preload.load(options.success)
+            }
+
+            //使用缓存
+            if (this.cache) {
+                var data = this.cache.get(key)
+                if (data) {
+                    return options.success.apply(null, data)
+                }
+            }
+            
+            return $.ajax(options)
+        }
+    }
+
+    return DataSource
+}))
---
+++ form.js
@@ -0,0 +1,107 @@
+﻿//表单处理
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['core'], factory)
+    } else {
+        factory($, core)
+    }
+}(function ($, core) {
+    'use strict'
+
+    var form = {
+        bind: function (data, contain, otherType) {
+            /// <summary>绑定表单</summary>
+            /// <param name="data" type="Object">实体对象</param>
+            /// <param name="contain" type="El">容器</param>      
+
+            var key,
+                item,
+                bindType = 'input,select,textarea' + (otherType ? ',' + otherType : '')
+                inputs = (contain || document).querySelectorAll(bindType)
+
+            inputs.forEach(function (input) {
+                var name = input.name || input.id,
+                    type = input.type,
+                    value,
+                    i,
+                    len,
+                    option
+
+                if (!name || !core.hasKey(data, name)) {
+                    return true
+                }
+
+                value = core.findValue(data, name) || ''
+
+                switch (type) {
+                    case 'select-multiple':
+                        input.options.forEach(function(option){
+                            option.selected = value.split(',').indexOf(option.value) > -1
+                        })                        
+                        break
+                    case 'checkbox':
+                    case 'radio':
+                        input.checked = (',' + value + ',').indexOf(',' + input.value + ',') > -1
+                        break
+                    default:
+                        if ('value' in input) {
+                            input.value = value
+                        } else {
+                            input.innerHTML = value
+                        }
+                        break
+                }
+
+            })
+        },
+        serialize: function (contain, encode) {
+            /// <summary>表单序列化</summary>
+            /// <param name="$contain" type="$">容器</param>
+            /// <param name="encode" type="Boolean">编码</param>
+
+            var fields = (contain || document).querySelectorAll('input,select,textarea'),
+                obj = {}
+
+            fields.forEach(function (input) {
+                var name = input.name || input.id,
+                    type = input.type,
+                    value
+
+                if (!name || input.getAttribute('noserialize') != null) {
+                    continue
+                }
+                
+                if('value' in input){
+                    value = input.value 
+                }
+
+                if (encode) {
+                    value = encodeURIComponent(value || '')
+                }
+
+                switch (input.type) {
+                    case 'select-multiple':
+                        value = input.options.map(function(option){
+                            return option.selected ? option.value : ''
+                        }).join(',')                      
+                    case 'checkbox':
+                    case 'radio':
+                        if (!input.checked) {
+                            break
+                        }
+                    default:
+                        var temp = obj
+                        if (name.indexOf('.') > -1) {
+                            temp = core.namespace(name, obj, true)
+                            name = name.split('.').pop()
+                        }
+                        temp[name] = temp[name] ? temp[name] + ',' + value : value
+                }
+            })
+
+            return obj
+        }
+    }
+
+    return form
+}))
